eaaa18a3d5ffdd99f021d2428203295b4e2df293
shyam-biradar <shyam.biradar@triliodata.com>
2016-10-21 05:44:55 -0400

    Fixed selective restore test case

--- tempest/api/workloadmgr/cli_automation/config/configuration.py
+++ tempest/api/workloadmgr/cli_automation/config/configuration.py
@@ -9,6 +9,7 @@ snapshot_name = "test2-snapshot"
 snapshot_type_full = "full"
 restore_name = "test-oneclick-restore"
 selective_restore_name = "test-selective-restore"
+restore_filename = "/opt/stack/python-workloadmgrclient/input-files/restore.json"
 workload_modify_name = "test2-new"
 workload_modify_description = "test2-new-description"
 
