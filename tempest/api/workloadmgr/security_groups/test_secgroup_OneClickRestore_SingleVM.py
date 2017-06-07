cbd8abd7883b014b32e6eff56adc2f4a0e170741
savitha <savita.peri@triliodata.com>
2017-06-07 07:44:51 -0400

    TVAULT-1734

---
+++ tempest/api/workloadmgr/security_groups/test_secgroup_OneClickRestore_SingleVM.py
@@ -0,0 +1,113 @@
+import sys
+import os
+sys.path.append(os.getcwd())
+from tempest.api.workloadmgr import base
+from tempest import config
+from tempest import test
+from oslo_log import log as logging
+from tempest import tvaultconf
+import time
+from tempest.api.workloadmgr.cli.config import command_argument_string
+from tempest.api.workloadmgr.cli.util import cli_parser, query_data
+
+LOG = logging.getLogger(__name__)
+CONF = config.CONF
+
+class RestoreTest(base.BaseWorkloadmgrTest):
+
+    credentials = ['primary']
+
+    @classmethod
+    def setup_clients(cls):
+        super(RestoreTest, cls).setup_clients()
+        cls.client = cls.os.wlm_client
+
+    @test.attr(type='smoke')
+    @test.idempotent_id('9fe07175-912e-49a5-a629-5f52eeada4c9')
+    def test_secgroup_OneClickRestore_SingleVM(self):
+        #Prerequisites
+        self.created = False
+        self.workload_instances = []
+        
+        #Create security groups
+        self.sec_grp_id_1 = self.create_security_group(tvaultconf.security_group_name + "_1")
+        self.add_security_rule(self.sec_grp_id_1, "TCP", 1, 65535)
+        
+        self.sec_grp_id_2 = self.create_security_group(tvaultconf.security_group_name + "_2")
+        self.add_security_rule(self.sec_grp_id_1, "TCP", 1, 65535, self.sec_grp_id_1)
+        
+        #Launch instance
+        self.vm_id = self.create_vm(vm_cleanup=False, security_group_id=self.sec_grp_id_1)
+        LOG.debug("VM ID: " + str(self.vm_id))
+        
+        
+
+        #Create volume
+        self.volume_id = self.create_volume(tvaultconf.volume_size,tvaultconf.volume_type, volume_cleanup=False)
+        LOG.debug("Volume ID: " + str(self.volume_id))
+        
+        #Attach volume to the instance
+        self.attach_volume(self.volume_id, self.vm_id, attach_cleanup=False)
+        LOG.debug("Volume attached")
+
+        #Create workload
+        self.workload_instances.append(self.vm_id)
+        self.wid = self.workload_create(self.workload_instances, tvaultconf.parallel, workload_name=tvaultconf.workload_name)
+        LOG.debug("Workload ID: " + str(self.wid))
+        time.sleep(5)
+        
+        #Create snapshot
+        self.snapshot_id = self.workload_snapshot(self.wid, True, tvaultconf.snapshot_name)
+        LOG.debug("Snapshot ID: " + str(self.snapshot_id))
+        
+        #Wait till snapshot is complete
+        self.wait_for_snapshot_tobe_available(self.wid, self.snapshot_id)
+                
+        #Delete the original instance
+        self.delete_vm(self.vm_id)
+        LOG.debug("Instance deleted successfully")
+        
+        #Delete corresponding volume
+        self.delete_volume(self.volume_id)
+        LOG.debug("Volume deleted successfully")
+        
+        #Create one-click restore using CLI command
+        restore_command = command_argument_string.oneclick_restore + " " + self.snapshot_id
+        rc = cli_parser.cli_returncode(restore_command)
+        if rc != 0:
+            raise Exception("Command did not execute correctly")
+        else:
+            LOG.debug("Command executed correctly")
+        
+        wc = query_data.get_snapshot_restore_status(tvaultconf.restore_name,self.snapshot_id)
+        LOG.debug("Snapshot restore status: " + str(wc))
+        while (str(wc) != "available" or str(wc)!= "error"):
+            time.sleep (5)
+            wc = query_data.get_snapshot_restore_status(tvaultconf.restore_name, self.snapshot_id)
+            LOG.debug("Snapshot restore status: " + str(wc))
+            if (str(wc) == "available"):
+                LOG.debug("Snapshot Restore successfully completed")
+                self.created = True
+                break
+            else:
+                if (str(wc) == "error"):
+                    break
+    
+        if (self.created == False):
+            raise Exception ("Snapshot Restore did not get created")
+        
+        self.restore_id = query_data.get_snapshot_restore_id(self.snapshot_id)
+        LOG.debug("Restore ID: " + str(self.restore_id))
+        
+        #Cleanup
+        #self.volume_snapshots = self.get_available_volume_snapshots()
+        #self.delete_volume_snapshots(self.volume_snapshots)
+        
+        #Delete restore for snapshot
+        self.restore_delete(self.wid, self.snapshot_id, self.restore_id)
+        LOG.debug("Snapshot Restore deleted successfully")
+               
+        #Delete restored VM instance and volume
+        #self.delete_restored_vms(self.restore_id)
+        #LOG.debug("Restored VM and volume deleted successfully")
+
