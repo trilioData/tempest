e5898eafc0f6c64a57e20ff5a5a091f78dd8b73b
deepanshusagar <deepanshu.sagar@trilio.io>
2017-08-08 14:56:51 +0530

    Update tvaultconf.py

--- tempest/tvaultconf.py
+++ tempest/tvaultconf.py
@@ -5,9 +5,6 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 # following cleanup parameter value to True otherwise False
 cleanup = True
 
-# pre requisite parameter
-pre_req = True
-
 #Test results for reporting
 PASS = "PASS"
 FAIL = "FAIL"
