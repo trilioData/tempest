74f872cc5daa2504acd1bb763b803f312af0b6ed
Deepanshu Sagar <deepanshu.sagar@triliodata.com>
2017-09-01 18:29:24 +0530

    indentation fixed

--- tempest/api/workloadmgr/data_integrity/test_tvault_test_inplace.py
+++ tempest/api/workloadmgr/data_integrity/test_tvault_test_inplace.py
@@ -54,13 +54,13 @@ class WorkloadsTest(base.BaseWorkloadmgrTest):
             self.md5sums_dir_before = tree()
             for floating_ip in self.floating_ips_list:
                 for mount_point in mount_points:
-	    	ssh = self.SshRemoteMachineConnectionWithRSAKey(str(floating_ip))
+	    	    ssh = self.SshRemoteMachineConnectionWithRSAKey(str(floating_ip))
                     self.addCustomSizedfilesOnLinux(ssh, mount_point, 1)
-	    	ssh.close()
+	    	    ssh.close()
 	        for mount_point in mount_points:
-	    	ssh = self.SshRemoteMachineConnectionWithRSAKey(str(floating_ip))
+	    	    ssh = self.SshRemoteMachineConnectionWithRSAKey(str(floating_ip))
                     self.md5sums_dir_before[str(floating_ip)][str(mount_point)] = self.calculatemmd5checksum(ssh, mount_point)
-	        	ssh.close()
+	      	    ssh.close()
 	    
 	    LOG.debug("md5sums_dir_before" + str(self.md5sums_dir_before))	
 	    #Create in-place restore with CLI command
@@ -121,9 +121,9 @@ class WorkloadsTest(base.BaseWorkloadmgrTest):
             md5_sum_after_in_place_restore = tree()
             for floating_ip in self.floating_ips_list:
                 for mount_point in mount_points:
-	    	ssh = self.SshRemoteMachineConnectionWithRSAKey(str(floating_ip))
+	    	    ssh = self.SshRemoteMachineConnectionWithRSAKey(str(floating_ip))
                     md5_sum_after_in_place_restore[str(floating_ip)][str(mount_point)] = self.calculatemmd5checksum(ssh, mount_point)
-	    	ssh.close()
+	    	    ssh.close()
 	    LOG.debug("md5_sum_after_in_place_restore" + str(md5_sum_after_in_place_restore))
 	    
 	    if(self.md5sums_dir_before == md5_sum_after_in_place_restore):
