a549f61303f9455398ccc118b4afe01406a3ac5b
savitha <savita.peri@triliodata.com>
2017-10-26 08:23:07 -0400

    license-check for capacity, compute licenses added

--- tempest/api/workloadmgr/license/test_license_check.py
+++ tempest/api/workloadmgr/license/test_license_check.py
@@ -22,11 +22,11 @@ class WorkloadTest(base.BaseWorkloadmgrTest):
     def setup_clients(cls):
         super(WorkloadTest, cls).setup_clients()
         cls.client = cls.os.wlm_client
-	reporting.add_test_script(str(__name__))
 
     @test.attr(type='smoke')
     @test.idempotent_id('9fe07175-912e-49a5-a629-5f52eeada4c9')
-    def test_license_check(self):
+    def test_license_check_vms(self):
+	reporting.add_test_script(str(__name__) + "_vms")
 	try:
 	    #Create license using CLI command
   	    self.cmd = command_argument_string.license_create + tvaultconf.vm_license_filename
@@ -70,3 +70,87 @@ class WorkloadTest(base.BaseWorkloadmgrTest):
 	    LOG.error("Exception: " + str(e))
             reporting.set_test_script_status(tvaultconf.FAIL)
             reporting.test_case_to_write()
+
+    @test.attr(type='smoke')
+    @test.idempotent_id('9fe07175-912e-49a5-a629-5f52eeada4c9')
+    def test_license_check_capacity(self):
+        reporting.add_test_script(str(__name__) + "_capacity")
+        try:
+            #Create license using CLI command
+            self.cmd = command_argument_string.license_create + tvaultconf.capacity_license_filename
+            rc = cli_parser.cli_returncode(self.cmd)
+            if rc != 0:
+                reporting.add_test_step("Apply 100GB license", tvaultconf.FAIL)
+                reporting.set_test_script_status(tvaultconf.FAIL)
+                raise Exception("Command did not execute correctly")
+            else:
+                reporting.add_test_step("Apply 100GB license", tvaultconf.PASS)
+
+            #Verify license-check CLI command
+            self.cmd = command_argument_string.license_check
+            rc = cli_parser.cli_returncode(self.cmd)
+            if rc != 0:
+                reporting.add_test_step("Execute license-check command", tvaultconf.FAIL)
+                raise Exception("Command did not execute correctly")
+            else:
+                reporting.add_test_step("Execute license-check command", tvaultconf.PASS)
+
+            #Verification
+            out = cli_parser.cli_output(self.cmd)
+            LOG.debug("CLI Response: " + str(out))
+            get_usage_tvault = "df -h | grep triliovault-mounts"
+            ssh = self.SshRemoteMachineConnection(tvaultconf.tvault_ip, tvaultconf.tvault_dbusername, tvaultconf.tvault_dbpassword)
+            stdin, stdout, stderr = ssh.exec_command(get_usage_tvault)
+            tmp = ' '.join(stdout.read().split())
+            usage = tmp.split(' ')
+            LOG.debug("Data from Tvault: " + str(usage) + " Usage: " + str(usage[2]))
+            ssh.close()
+            if(str(out).find(usage[2]) != -1):
+                reporting.add_test_step("License-check verification", tvaultconf.PASS)
+            else:
+                reporting.add_test_step("License-check verification", tvaultconf.FAIL)
+                raise Exception("License-check verification failed")
+            reporting.test_case_to_write()
+        except Exception as e:
+            LOG.error("Exception: " + str(e))
+            reporting.set_test_script_status(tvaultconf.FAIL)
+            reporting.test_case_to_write()
+
+    @test.attr(type='smoke')
+    @test.idempotent_id('9fe07175-912e-49a5-a629-5f52eeada4c9')
+    def test_license_check_compute(self):
+        reporting.add_test_script(str(__name__) + "_compute")
+        try:
+            #Create license using CLI command
+            self.cmd = command_argument_string.license_create + tvaultconf.compute_license_filename
+            rc = cli_parser.cli_returncode(self.cmd)
+            if rc != 0:
+                reporting.add_test_step("Apply 10 compute node license", tvaultconf.FAIL)
+                reporting.set_test_script_status(tvaultconf.FAIL)
+                raise Exception("Command did not execute correctly")
+            else:
+                reporting.add_test_step("Apply 10 compute node license", tvaultconf.PASS)
+
+            #Verify license-check CLI command
+            self.cmd = command_argument_string.license_check
+            rc = cli_parser.cli_returncode(self.cmd)
+            if rc != 0:
+                reporting.add_test_step("Execute license-check command", tvaultconf.FAIL)
+                raise Exception("Command did not execute correctly")
+            else:
+                reporting.add_test_step("Execute license-check command", tvaultconf.PASS)
+
+            #Verification
+            out = cli_parser.cli_output(self.cmd)
+            LOG.debug("CLI Response: " + str(out))
+            if(str(out).find('Number of compute nodes deployed \'1\'') != -1):
+                reporting.add_test_step("License-check verification", tvaultconf.PASS)
+            else:
+                reporting.add_test_step("License-check verification", tvaultconf.FAIL)
+                raise Exception("License-check verification failed")
+            reporting.test_case_to_write()
+        except Exception as e:
+            LOG.error("Exception: " + str(e))
+            reporting.set_test_script_status(tvaultconf.FAIL)
+            reporting.test_case_to_write()
+
