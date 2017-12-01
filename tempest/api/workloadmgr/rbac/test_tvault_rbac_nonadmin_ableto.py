1cf748cdfab96918acc17d72fb3fa9afb304b106
deepanshu <deepanshu.sagar@github.com>
2017-12-01 11:34:51 +0530

    rbac testfile1 added and commited

---
+++ tempest/api/workloadmgr/rbac/test_tvault_rbac_nonadmin_ableto.py
@@ -0,0 +1,138 @@
+import sys
+import os
+sys.path.append(os.getcwd())
+from tempest.api.workloadmgr import base
+from tempest import config
+from tempest import test
+from oslo_log import log as logging
+from tempest import tvaultconf
+from tempest import reporting
+from tempest import command_argument_string
+from tempest.util import cli_parser
+from tempest.util import query_data
+import time
+
+LOG = logging.getLogger(__name__)
+CONF = config.CONF
+
+class WorkloadsTest(base.BaseWorkloadmgrTest):
+
+    credentials = ['primary']
+
+    @classmethod
+    def setup_clients(cls):
+        super(WorkloadsTest, cls).setup_clients()
+        cls.client = cls.os.wlm_client
+	reporting.add_test_script(str(__name__))
+
+    @test.attr(type='smoke')
+    @test.idempotent_id('9fe07175-912e-49a5-a629-5f52eeada4c9')
+    def test_tvault_rbac_nonadmin_ableto(self):
+	try:
+	    # Use non-admin credentials
+	    os.environ['OS_USERNAME']= 'nonadmin'
+            os.environ['OS_PASSWORD']= 'password'
+	    self.instances_id = []
+
+	    # Create volume, Launch an Instance
+	    floating_ips_list = self.get_floating_ips()
+	    self.volumes_id = self.create_volume(volume_cleanup=False)
+            LOG.debug("Volume-"+ str(1) +" ID: " + str(self.volumes_id))
+            self.instances_id.append(self.create_vm(vm_cleanup=False, key_pair=tvaultconf.key_pair_name))
+            LOG.debug("VM-"+ str(1) +" ID: " + str(self.instances_id[0]))
+            self.attach_volume(self.volumes_id, self.instances_id[0])
+            LOG.debug("Volume attached")
+            self.set_floating_ip(floating_ips_list[0], self.instances_id[0])
+
+	    # Create workload
+    	    self.wid = self.workload_create(self.instances_id, tvaultconf.parallel, workload_name=tvaultconf.workload_name)
+            LOG.debug("Workload ID: " + str(self.wid))
+            workload_available = self.wait_for_workload_tobe_available(self.wid)	    
+            if workload_available == True:
+	        LOG.debug("Workload created successfully")
+                reporting.add_test_step("Verification of workload creation", tvaultconf.PASS)
+                reporting.set_test_script_status(tvaultconf.PASS)
+	    else:
+		LOG.debug("workload creation unsuccessful")
+                reporting.add_test_step("Verification of workload creation", tvaultconf.FAIL)
+                raise Exception ("RBAC policy fails for workload creation by non-admin user")
+
+	    # Create full snapshot 
+    	    self.snapshot_id = self.workload_snapshot(self.wid, True)
+    	    LOG.debug("Snapshot ID-1: " + str(self.snapshot_id))
+            #Wait till snapshot is complete
+            snapshot_status  = self.wait_for_snapshot_tobe_available(self.wid, self.snapshot_id)    
+            if snapshot_status == "available":
+                LOG.debug("snapshot created successfully")
+                reporting.add_test_step("Verification of snapshot creation", tvaultconf.PASS)
+                reporting.set_test_script_status(tvaultconf.PASS)
+            else:
+                LOG.debug("snapshot creation unsuccessful")
+                reporting.add_test_step("Verification of snapshot creation", tvaultconf.FAIL)
+                raise Exception ("RBAC policy fails for snapshot creation by non-admin user")
+
+	    #Delete the original instance
+            self.delete_vm(self.instances_id[0])
+            LOG.debug("Instance deleted successfully")
+        
+            #Delete corresponding volume
+            self.delete_volume(self.volumes_id)
+            LOG.debug("Volume deleted successfully")
+
+            #Create one-click restore
+	    restore_status = ""
+            restore_name="restore_1"
+            restore_id = self.snapshot_restore(self.wid, self.snapshot_id, restore_name=restore_name)
+            restore_status = query_data.get_snapshot_restore_status(restore_name, self.snapshot_id)
+            LOG.debug("Snapshot restore status initial: " + str(restore_status))
+            while (str(restore_status) != "available" and str(restore_status)!= "error"):
+                time.sleep (10)
+                restore_status = query_data.get_snapshot_restore_status(restore_name, self.snapshot_id)
+                LOG.debug("Snapshot restore status: " + str(restore_status))
+            if (str(restore_status) == "available"):
+                LOG.debug("Snapshot Restore successfully completed")
+                reporting.add_test_step("Snapshot one-click restore verification with DB", tvaultconf.PASS)
+            else:
+                LOG.debug("Snapshot Restore unsuccessful")
+                reporting.add_test_step("Snapshot one-click restore verification with DB", tvaultconf.FAIL)
+
+	    # Launch recovery instance and Mount snapshot
+	    self.recoveryinstances_id = self.create_vm(flavor_id=tvaultconf.recovery_flavor_ref, key_pair=tvaultconf.key_pair_name, image_id=tvaultconf.recovery_image_ref)
+            LOG.debug("VM-"+ str(2) +" ID: " + str(self.recoveryinstances_id))
+	    status = self.mount_snapshot(self.snapshot_id, self.recoveryinstances_id)
+            if status == True:
+                LOG.debug("snapshot Mounted successfully")
+		reporting.add_test_step("Verification of snapshot mount", tvaultconf.PASS)
+            else:
+		LOG.debug("snapshot Mount unsuccessful")
+                reporting.add_test_step("Verification of snapshot mount", tvaultconf.FAIL)
+		raise Exception ("snapshot does not Mount by non-admin user")
+	    
+
+	    # Run Filesearch
+	    vmid_to_search = self.instances_id[0]
+	    filepath_to_search = "/File_1.txt"
+	    filecount_in_snapshots = {self.snapshot_id: 0}
+	    filesearch_id = self.filepath_search(vmid_to_search, filepath_to_search)
+	    snapshot_wise_filecount = self.verifyFilepath_Search(filesearch_id, filepath_to_search)
+	    for snapshot_id in filecount_in_snapshots.keys():
+    	        if snapshot_wise_filecount[snapshot_id] == filecount_in_snapshots[snapshot_id]:
+		    filesearch_status = True
+	        else:
+		    filesearch_status = False
+		    LOG.debug("Filepath Search unsuccessful")
+                    reporting.add_test_step("Verification of Filepath serach", tvaultconf.FAIL)
+	   	    raise Exception ("Filesearch path does not execute correctly by non-admin user")
+
+	    if filesearch_status == True:
+	        LOG.debug("Filepath_Search successful")
+	    	reporting.add_test_step("Verification of Filepath serach", tvaultconf.PASS)
+
+	    reporting.set_test_script_status(tvaultconf.PASS)
+            reporting.test_case_to_write()
+
+        except Exception as e:
+            LOG.error("Exception: " + str(e))
+            reporting.set_test_script_status(tvaultconf.FAIL)
+            reporting.test_case_to_write()
+
