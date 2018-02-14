ed81dfc29e97dcb36946605fda4156e90d335dae
root <root@localhost.localdomain>
2018-02-14 18:09:55 +0530

    chhanges in master_run

--- master-run.sh
+++ master-run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
 BASE_DIR="$(pwd)"
-source automation/openstack-build-scripts/build.properties
+#source automation/openstack-build-scripts/build.properties
 #source automation/openstack-build-scripts/openstack-auth.sh
 
 TEST_LIST_FILE="$BASE_DIR/test-list"
 TEST_RESULTS_FILE="$BASE_DIR/test_results"
-SUITE_LIST=("tempest.api.workloadmgr.data_integrity")
+SUITE_LIST=("tempest.api.workloadmgr.regression")
+TEST_CASES_LIST=("")
 REPORT_DIR="$BASE_DIR/Report"
 
 #Clean old files
@@ -19,6 +20,12 @@ python -c 'from tempest import reporting; reporting.consolidate_report_table()'
 
 for suite in "${SUITE_LIST[@]}"
 do
+    if [[ *"snapshot"* = $suite || *"config"* = $suite || *"filesearch"* = $suite ]]; then
+	TEST_CASES_LIST=("tempest.api.workkloadmgr.snapshot.test_bootfromvol_fullsnapshot" "tempest.api.workkloadmgr.snapshot.test_snapshots" "tempest.api.workloadmgr.filesearch.test_filesearch" "config_backup.test_config_workload" "tempest.api.workloadmgr.config_backup.test_config_backup")
+    fi
+
+    echo $TEST_CASES_LIST
+
     testname=$(echo $suite| cut -d'.' -f 4)
     python -c "from tempest import reporting; reporting.setup_report('$testname')"
     tools/with_venv.sh ./run_tempest.sh --list-tests $suite > $TEST_LIST_FILE
@@ -31,7 +38,7 @@ do
         LOGS_DIR=`echo "$line" | sed  's/\./\//g'`
         LOGS_DIR=logs/$LOGS_DIR
         mkdir -p $LOGS_DIR
-        ./run_tempest.sh -V $line
+        #./run_tempest.sh -V $line
         if [ $? -eq 0 ]; then
 	       echo "$line PASSED" >> $TEST_RESULTS_FILE
         else
@@ -40,6 +47,28 @@ do
         mv -f tempest.log $LOGS_DIR/
     
     
+    done < "$TEST_LIST_FILE"
+    python -c 'from tempest import reporting; reporting.end_report_table()'
+done
+
+for test_case in "${TEST_CASES_LIST[@]}"
+do
+    python -c "from tempest import reporting; reporting.setup_report('$test_case')"
+    while read -r line
+    do
+        rm -rf /opt/lock
+        LOGS_DIR=`echo "$test_case" | sed  's/\./\//g'`
+        LOGS_DIR=logs/$LOGS_DIR
+        mkdir -p $LOGS_DIR
+        #./run_tempest.sh -V $test_case
+        if [ $? -eq 0 ]; then
+               echo "$test_case PASSED" >> $TEST_RESULTS_FILE
+        else
+               echo "$test_case FAILED" >> $TEST_RESULTS_FILE
+        fi
+        mv -f tempest.log $LOGS_DIR/
+
+
     done < "$TEST_LIST_FILE"
     python -c 'from tempest import reporting; reporting.end_report_table()'
 done
