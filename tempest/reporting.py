55da33907bcc8bea35eb90e8b6d93eafcabbfd40
deepanshu <deepanshu.sagar@trilio.io>
2018-02-16 11:43:41 +0530

    get_tests

--- tempest/reporting.py
+++ tempest/reporting.py
@@ -113,11 +113,13 @@ def consolidate_report_table():
 def add_sanity_results(test_step, status):
     with open(sanity_results_file, "a") as f:
 	    f.write(str(test_step) + " " + str(status) + "\n")
+
 def get_tests(suite_path):
     import glob
     print suite_path
     print str(suite_path)+"/*.py" 
-    for path in glob.glob(str(suite_path)+"*.py"):
+    for path in glob.glob(str(suite_path)+"/*.py"):
+	print "inside loop"
 	print path
 	if "__init__" not in path:
 	    print test_list_file
