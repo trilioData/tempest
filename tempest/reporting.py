2e63e9092742de1659325e63da8b6f6f4d711ea1
deepanshu <deepanshu.sagar@trilio.io>
2018-02-16 17:06:22 +0530

    get_tests import path changed

--- tempest/reporting.py
+++ tempest/reporting.py
@@ -112,3 +112,11 @@ def consolidate_report_table():
 def add_sanity_results(test_step, status):
     with open(sanity_results_file, "a") as f:
 	    f.write(str(test_step) + " " + str(status) + "\n")
+
+def get_tests(test_list_file,suite_path):
+    import glob
+    for path in glob.glob(str(suite_path)+"/*.py"):
+        if "__init__" not in path:
+            with open (test_list_file, "a") as f:
+                print "test: " + ".".join(str(path[:-3]).split("/")[6:])+"\n"
+                f.write(".".join(str(path[:-3]).split("/")[6:])+"\n")
