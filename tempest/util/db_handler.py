4d95f529fe1be3a98c2837e36364cd6f92d20061
savitha <savita.peri@triliodata.com>
2019-01-16 00:30:28 -0500

    Fixed db passwd

--- tempest/util/db_handler.py
+++ tempest/util/db_handler.py
@@ -3,7 +3,7 @@ import mysql.connector
 from tempest import tvaultconf
 import paramiko
 
-def get_db_pass(hostip=tvaultconf.tvault_ip,user=tvaultconf.tvault_dbusername,
+def get_db_credentials(hostip=tvaultconf.tvault_ip,user=tvaultconf.tvault_dbusername,
                 passwd=tvaultconf.tvault_dbpassword):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -11,14 +11,14 @@ def get_db_pass(hostip=tvaultconf.tvault_ip,user=tvaultconf.tvault_dbusername,
     ssh.connect(hostname=hostip, username=user, password=passwd)
     command = "cat /etc/workloadmgr/workloadmgr.conf | grep 'sql_connection' | cut -d '/' -f 3 | cut -d '@' -f 1"
     stdin, stdout, stderr = ssh.exec_command(command)
-    pwd = stdout.read()
-    return pwd
+    creds = stdout.read()
+    return creds
 
 def dbHandler():
     try:        
-	pwd = (str(get_db_pass(hostip=tvaultconf.tvault_ip)).split("\n"))[0].split(":")
-        conn = mysql.connector.connect(user=tvaultconf.tvault_dbusername,password=pwd,
-                                       host=tvaultconf.tvault_ip,database=tvaultconf.tvault_dbname)
+	cred = (str(get_db_credentials(hostip=tvaultconf.tvault_ip)).split("\n"))[0].split(":")
+        conn = mysql.connector.connect(user=cred[0],password=cred[1],host=tvaultconf.tvault_ip,
+					database=tvaultconf.tvault_dbname)
         print conn
         return conn
     except mysql.connector.Error as err:
