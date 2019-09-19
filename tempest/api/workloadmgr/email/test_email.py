8d4fe8ba03d7077fb5125d0f3ce5656d10dd4da9
nikhil-achole <nikhil-achole@trilio.io>
2019-09-19 03:24:55 -0400

    Verification part corrected

--- tempest/api/workloadmgr/email/test_email.py
+++ tempest/api/workloadmgr/email/test_email.py
@@ -42,10 +42,8 @@ class WorkloadTest(base.BaseWorkloadmgrTest):
                     if (self.delete_setting(k) == False):
                         flag = True
             if flag:
-
                 reporting.add_test_step("Delete existing setting", tvaultconf.FAIL)
             else:
-                
                 #Update trilioVault email settings
                 settings_resp = self.update_email_setings(tvaultconf.setting_data)
                 setting_data_from_resp = {}
@@ -61,28 +59,27 @@ class WorkloadTest(base.BaseWorkloadmgrTest):
                     enable_email_resp = self.update_email_setings(tvaultconf.enable_email_notification)[0]
                     if((str(enable_email_resp['name']) == 'smtp_email_enable') and (str(enable_email_resp['value']) == '1')):
                         reporting.add_test_step("Enable email notification for project", tvaultconf.PASS)
+                        self.wlm_client.client.get("/workloads/email/test_email")
+
+                        cmd = 'curl  -u '+tvaultconf.setting_data["smtp_default_recipient"]+':'+tvaultconf.tvault_password+' --silent "https://mail.google.com/mail/feed/atom"'
+                        op = subprocess.check_output(cmd, shell=True)
+                        if len(re.findall('Testing email configuration', op.split('<entry>')[1])) == 1:
+                            LOG.debug("Email testing done correctly and email is : {}".format(op))
+                            reporting.add_test_step("Test email", tvaultconf.PASS)
+                        else:
+                            reporting.add_test_step("Test email", tvaultconf.FAIL)
+                        existing_setting = self.get_settings_list()
+                        LOG.debug("Existing setting list: " + str(existing_setting))
+
+                        #Delete the existing settings
+                        for k,v in existing_setting.items():
+                            self.delete_setting(k)
                     else:
                         reporting.add_test_step("Enable email notification for project", tvaultconf.FAIL)
                         reporting.set_test_script_status(tvaultconf.FAIL)
-
                 else:
                     reporting.add_test_step("Update email settings", tvaultconf.FAIL)
                     reporting.set_test_script_status(tvaultconf.FAIL)
-
-            self.wlm_client.client.get("/workloads/email/test_email")
-            
-            cmd = 'curl  -u '+tvaultconf.setting_data["smtp_default_recipient"]+':'+tvaultconf.tvault_password+' --silent "https://mail.google.com/mail/feed/atom"'
-            op = subprocess.check_output(cmd, shell=True)
-            if len(re.findall('Testing email configuration', op.split('<entry>')[1])) == 1:
-                LOG.debug("Email testing done correctly and email is : {}".format(op))
-                reporting.add_test_step("Test email", tvaultconf.PASS)
-            else:
-                reporting.add_test_step("Test email", tvaultconf.FAIL)
-            existing_setting = self.get_settings_list()
-            LOG.debug("Existing setting list: " + str(existing_setting))
-            #Delete any existing settings
-            for k,v in existing_setting.items():
-                self.delete_setting(k)
 
             reporting.test_case_to_write() 
 
