dacaf6d55fd2c06fa7acef5b61e8e009102bbff5
savitha <savitha.peri@trilio.io>
2020-06-19 14:29:14 +0530

    Fixed imports

--- tempest/api/workloadmgr/scheduler/_test_verify_tvault1265.py
+++ tempest/api/workloadmgr/scheduler/_test_verify_tvault1265.py
@@ -16,7 +16,7 @@ import sys
 import os
 from tempest.api.workloadmgr import base
 from tempest import config
-from tempest import test
+from tempest.lib import decorators
 import json
 import datetime
 import time
@@ -37,10 +37,9 @@ class WorkloadsTest(base.BaseWorkloadmgrTest):
     @classmethod
     def setup_clients(cls):
         super(WorkloadsTest, cls).setup_clients()
-        cls.client = cls.os.wlm_client
 
-    @test.attr(type='smoke')
-    @test.idempotent_id('9fe07175-912e-49a5-a629-5f52eeada4c9')
+    @decorators.attr(type='smoke')
+    @decorators.idempotent_id('9fe07175-912e-49a5-a629-5f52eeada4c9')
     def test_create_workload(self):
         self.snap_list = []
         self.snapshot_type = []
