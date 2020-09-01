21f393ec14fa62c8775ae4a6d26e209250068ac9
Savitha <savitha.peri@trilio.io>
2020-09-01 16:02:27 +0530

    Fixed db pwd

--- tempest/util/db_handler.py
+++ tempest/util/db_handler.py
@@ -11,16 +11,16 @@ def get_db_credentials(hostip=tvaultconf.tvault_ip[0], user=tvaultconf.tvault_db
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     # ssh.load_system_host_keys()
     ssh.connect(hostname=hostip, username=user, password=pwd)
-    command = "cat /etc/workloadmgr/workloadmgr.conf | grep 'sql_connection' | cut -d '/' -f 3 | cut -d '@' -f 1"
+    command = "cat /root/.my.cnf | grep password | cut -d '=' -f 2"
     stdin, stdout, stderr = ssh.exec_command(command)
-    credentials = stdout.read()
+    credentials = stdout.readlines()[0].rstrip()
+    print(credentials)
     return credentials
 
 
 def dbHandler():
     try:
-        creds = get_db_credentials(hostip=tvaultconf.tvault_ip[0])
-        pwd = str(creds).split(':')[1].replace('\\n\'','')
+        pwd = get_db_credentials(hostip=tvaultconf.tvault_ip[0])
         conn = mysql.connector.connect(user=tvaultconf.tvault_dbusername, password=pwd,
                                        host=tvaultconf.tvault_ip[0], database=tvaultconf.tvault_dbname)
         print(conn)
