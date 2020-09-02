77f6b7eb60c1f929ef4b46e8ba61dbc21cd19854
Savitha <savitha.peri@trilio.io>
2020-09-02 14:03:05 +0530

    Fixed venv dependencies

--- tools/install_venv_common.py
+++ tools/install_venv_common.py
@@ -95,9 +95,9 @@ class InstallVenv(object):
             print(self.py_version)
             print('Creating venv...', end=' ')
             if self.py_version == "python2.7":
-                self.run_command(['virtualenv', '-q','-p', self.py_version, '--system-site-packages',  self.venv])
+                self.run_command(['virtualenv', '-q','-p', self.py_version, self.venv])
             else:
-                self.run_command(['python3', '-q', '-m', 'venv', '--system-site-packages', self.venv])
+                self.run_command(['python3', '-q', '-m', 'venv', self.venv])
             print('done.')
         else:
             print("venv already exists...")
