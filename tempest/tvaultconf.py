ddcd4bcfc08ee84e9361867716413a1e4b6cf56d
Savitha Peri <savitha.peri@trilio.io>
2021-02-08 14:16:47 +0200

    Fixed test email (#184) (#186)
    * fixed test email
    * fixes for juju

--- tempest/tvaultconf.py
+++ tempest/tvaultconf.py
@@ -47,6 +47,9 @@ tvault_ip = ["192.168.6.17", "192.168.6.18", "192.168.6.19"]
 tvault_dbusername = "root"
 tvault_dbname = "workloadmgr"
 tvault_password = "sample-password"
+wlm_dbusername = "workloadmgr"
+wlm_dbpasswd = "sample-password"
+wlm_dbhost = "192.168.6.17"
 
 no_of_compute_nodes = 1
 
