21f9db3593f26ebe525110418608be3d5b52bb10
Savitha Peri <savitha.peri@trilio.io>
2021-02-08 14:14:55 +0200

    Fixed test email (#185)
    * fixed test email
    * fixes for juju

--- tempest/util/db_handler.py
+++ tempest/util/db_handler.py
@@ -1,29 +1,13 @@
 from mysql.connector import errorcode
 import mysql.connector
 from tempest import tvaultconf
-import paramiko
-
-
-def get_db_credentials(hostip=tvaultconf.tvault_ip[0], user=tvaultconf.tvault_dbusername,
-                       pwd=tvaultconf.tvault_password):
-
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    # ssh.load_system_host_keys()
-    ssh.connect(hostname=hostip, username=user, password=pwd)
-    command = "cat /root/.my.cnf | grep password | cut -d '=' -f 2"
-    stdin, stdout, stderr = ssh.exec_command(command)
-    credentials = stdout.readlines()[0].rstrip()
-    print(credentials)
-    return credentials
-
 
 def dbHandler():
     try:
-        pwd = get_db_credentials(hostip=tvaultconf.tvault_ip[0])
-        conn = mysql.connector.connect(user=tvaultconf.tvault_dbusername, password=pwd,
-                                       host=tvaultconf.tvault_ip[0], database=tvaultconf.tvault_dbname)
-        print(conn)
+        conn = mysql.connector.connect(user=tvaultconf.wlm_dbusername,
+                                       password=tvaultconf.wlm_dbpasswd,
+                                       host=tvaultconf.wlm_dbhost,
+                                       database=tvaultconf.tvault_dbname)
         return conn
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -32,7 +16,6 @@ def dbHandler():
             print("Database does not exist")
         else:
             print(err)
-
 
 def closeDbConnection():
     try:
