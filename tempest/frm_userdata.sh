570e58a5b6fb48544d7ac52c1db0019ec859f659
savitha.peri <savitha.peri@trilio.io>
2022-01-06 11:10:07 +0530

    frm userdata updated

--- tempest/frm_userdata.sh
+++ tempest/frm_userdata.sh
@@ -3,12 +3,13 @@
 DISTRO=$(awk '/ID=/' /etc/os-release | sed 's/ID=//' | sed -r 's/\"|\(|\)//g' | awk '{print $1;exit}')
 if [[ "$DISTRO" == "rhel" || "$DISTRO" == "centos" ]]; then
     echo "rhel/centos based FRM image"
+    sudo su
     sed -i 's/guest-file-open,//' /etc/sysconfig/qemu-ga
     sed -i 's/guest-file-write,//' /etc/sysconfig/qemu-ga
     sed -i 's/guest-file-read,//' /etc/sysconfig/qemu-ga
     sed -i 's/guest-file-close,//' /etc/sysconfig/qemu-ga
     sed -i '/SELINUX=enforcing/c SELINUX=disabled' /etc/selinux/config
-    echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+    echo "nameserver 8.8.8.8" > /etc/resolv.conf
     sleep 50
     yum install python3 lvm2 -y 
     reboot
