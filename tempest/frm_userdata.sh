97cd06c8043dc670dbb3fbfb8780cd05aef4efe6
Priyanka Gupte <priyanka.gupte@trilio.io>
2022-04-12 06:41:49 -0700

    Updated frm_userdata.sh to handle ubuntu 20.4

--- tempest/frm_userdata.sh
+++ tempest/frm_userdata.sh
@@ -7,11 +7,11 @@ if [[ "$DISTRO" == "rhel" || "$DISTRO" == "centos" ]]; then
     NAME=$(awk '/^NAME=/' /etc/os-release | sed 's/NAME=//' | sed -r 's/\"|\(|\)//g')
     if grep -q "Stream" <<< "$NAME"
     then
-	echo "centos8 stream image"
+	    echo "centos8 stream image"
     else
-	echo "centos8 image"
-	sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
-        sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
+      echo "centos8 image"
+      sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
+      sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
     fi
     sed -i 's/guest-file-open,//' /etc/sysconfig/qemu-ga
     sed -i 's/guest-file-write,//' /etc/sysconfig/qemu-ga
@@ -30,7 +30,15 @@ else
     apt-get update
     apt-get install qemu-guest-agent
     systemctl enable qemu-guest-agent
-    sed -i '/DAEMON_ARGS=/c DAEMON_ARGS="-F/etc/qemu/fsfreeze-hook"' /etc/init.d/qemu-guest-agent
+    path=$(systemctl status qemu-guest-agent | grep Loaded)
+    if [[ $path =~ .*"/etc/init.d/qemu-guest-agent".* ]]
+    then
+      sed -i '/DAEMON_ARGS=/c DAEMON_ARGS="-F/etc/qemu/fsfreeze-hook"' /etc/init.d/qemu-guest-agent
+    elif [[ $path =~ .*"/lib/systemd/system/qemu-guest-agent.service".* ]]
+    then
+      mkdir -p /etc/systemd/system/qemu-guest-agent.service.d
+      echo -e "[Service]\nExecStart=\nExecStart=/usr/sbin/qemu-ga -F/etc/qemu/fsfreeze-hook" >> /etc/systemd/system/qemu-guest-agent.service.d/override.conf
+    fi
     systemctl daemon-reload
     systemctl restart qemu-guest-agent
     apt-get install python3
