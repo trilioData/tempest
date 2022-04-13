7d5fdfeb0517e64e39999866a220ebc99949a0c9
Priyanka Gupte <priyanka.gupte@trilio.io>
2022-04-13 01:19:10 -0700

    incorporated review comments

--- fetch_resources.sh
+++ fetch_resources.sh
@@ -434,7 +434,7 @@ function configure_tempest
               gateway_info=`($OPENSTACK_CMD router show $router_id | grep external_gateway_info | awk -F'|' '!/^(+--)|ID|aki|ari/ { print $3 }')`
               interface_info=`($OPENSTACK_CMD router show $router_id | grep interfaces_info | awk -F'|' '!/^(+--)|ID|aki|ari/ { print $3 }')`
               
-              if [[ $gateway_info == *"None"* ]] | [ $gateway_info == "null" ]
+              if [[ "$gateway_info" == *"None"* ]] | [ "$gateway_info" == "null" ]
               then
                   echo "External gateway not set"
                   output=$($OPENSTACK_CMD router set --external-gateway $ext_network_id $router_id 2>&1)
