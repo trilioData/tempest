1d8439f0c5681b2c0da38113eb9bb3e1b43c1335
savitha.peri <savitha.peri@trilio.io>
2022-06-05 12:34:19 +0530

    update

--- tools/with_venv.sh
+++ tools/with_venv.sh
@@ -3,4 +3,4 @@ TOOLS_PATH=${TOOLS_PATH:-$(dirname $0)/../}
 VENV_PATH=${VENV_PATH:-${TOOLS_PATH}}
 VENV_DIR=${VENV_DIR:-/.venv}
 VENV=${VENV:-${VENV_PATH}/${VENV_DIR}}
-source ${VENV}/bin/activate && "$@"
+source ${VENV}/bin/activate && cd /root && eval "$(<env.sh)" && cd - && "$@"
