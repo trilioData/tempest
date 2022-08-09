4311cd8dd331cdc7059b08cbb4fe08a558292765
savitha.peri <savitha.peri@trilio.io>
2022-08-09 10:39:34 +0530

    added project id to list sec groups

--- tempest/api/workloadmgr/base.py
+++ tempest/api/workloadmgr/base.py
@@ -3603,15 +3603,17 @@ class BaseWorkloadmgrTest(tempest.test.BaseTestCase):
         finally:
             return restored_secgrps
 
-    def list_security_groups(self):
-        body = self.security_groups_client.list_security_groups()
+    def list_security_groups(self, tenant_id=CONF.identity.tenant_id):
+        body = self.security_groups_client.list_security_groups(
+                tenant_id=tenant_id)
         security_groups = body["security_groups"]
         LOG.debug("No. of security groups: {}".format(len(security_groups)))
         LOG.debug("List of security groups: {}".format(security_groups))
         return security_groups
 
-    def list_security_group_rules(self):
-        body = self.security_group_rules_client.list_security_group_rules()
+    def list_security_group_rules(self, tenant_id=CONF.identity.tenant_id):
+        body = self.security_group_rules_client.list_security_group_rules(
+                tenant_id=tenant_id)
         rules_list = body["security_group_rules"]
         LOG.debug("No. of security group rules: {}".format(len(rules_list)))
         return rules_list
