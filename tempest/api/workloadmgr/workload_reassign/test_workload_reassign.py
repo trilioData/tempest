c25bcab12fe5a77510b540c80574c3a1a272c22f
SuhasT <suhas.tawade@trilio.io>
2022-08-24 13:06:17 +0530

    AUTO-177 : Workload Reassignment - OS:2058 & OS:2059 - stable/5.0

--- tempest/api/workloadmgr/workload_reassign/test_workload_reassign.py
+++ tempest/api/workloadmgr/workload_reassign/test_workload_reassign.py
@@ -22,12 +22,41 @@ class WorkloadTest(base.BaseWorkloadmgrTest):
     @classmethod
     def setup_clients(cls):
         super(WorkloadTest, cls).setup_clients()
-        reporting.add_test_script(str(__name__))
 
-    @decorators.idempotent_id('9fe07175-912e-49a5-a629-5f52eeada4c9')
+
+    def create_snapshot(self, workload_id, is_full=True):
+        if is_full:
+            substitution = 'Full'
+        else:
+            substitution = 'Incremental'
+
+        snapshot_id, command_execution, snapshot_execution = self.workload_snapshot_cli(
+            workload_id, is_full=is_full)
+        if command_execution == 'pass':
+            reporting.add_test_step("{} snapshot command execution".format(
+                substitution), tvaultconf.PASS)
+            LOG.debug("Command executed correctly for full snapshot")
+        else:
+            reporting.add_test_step("{} snapshot command execution".format(
+                substitution), tvaultconf.FAIL)
+            raise Exception(
+                "Command did not execute correctly for full snapshot")
+
+        if snapshot_execution == 'pass':
+            reporting.add_test_step("{} snapshot".format(
+                substitution), tvaultconf.PASS)
+        else:
+            reporting.add_test_step("{} snapshot".format(
+                substitution), tvaultconf.FAIL)
+            raise Exception("Full snapshot failed")
+
+        return (snapshot_id)
+
+
     @decorators.attr(type='workloadmgr_api')
-    def test_workload_reassign(self):
+    def test_01_workload_reassign(self):
         try:
+            reporting.add_test_script(str(__name__) + "_different_user_different_tenant")
             ### Create vm and workload ###
             self.created = False
             vm_id = self.create_vm(vm_cleanup=True)
@@ -78,3 +107,155 @@ class WorkloadTest(base.BaseWorkloadmgrTest):
             LOG.error("Exception: " + str(e))
             reporting.set_test_script_status(tvaultconf.FAIL)
             reporting.test_case_to_write()
+
+
+    '''
+    OS-2058 - workload reassign with same user and same project.
+    http://192.168.15.51/testlink/linkto.php?tprojectPrefix=OS&item=testcase&id=OS-2058
+    '''
+    @decorators.attr(type='workloadmgr_api')
+    def test_02_workload_reassign(self):
+        try:
+
+            reporting.add_test_script(str(__name__) + "_same_user_same_tenant")
+
+            ### Create vm and workload ###
+            LOG.debug("Create VM")
+
+            self.created = False
+            vm_id = self.create_vm(vm_cleanup=True)
+            LOG.debug("\nVm id : {}\n".format(str(vm_id)))
+
+            LOG.debug("Create workload")
+            workload_id = self.workload_create(
+                [vm_id], tvaultconf.parallel, workload_cleanup=True)
+            LOG.debug("Workload ID: " + str(workload_id))
+
+            if(workload_id is not None):
+                self.wait_for_workload_tobe_available(workload_id)
+                if(self.getWorkloadStatus(workload_id) == "available"):
+                    reporting.add_test_step("Create workload", tvaultconf.PASS)
+                else:
+                    LOG.error("Failed to get workload status as available for workload ID: " + str(workload_id))
+                    raise Exception("Create workload")
+            else:
+                LOG.error("Failed to create workload.")
+                raise Exception("Create workload")
+
+            #take a full snapshot of it.
+            snapshot_id = self.create_snapshot(workload_id, is_full=True)
+
+            snapshot_status = self.getSnapshotStatus(workload_id, snapshot_id)
+
+            if (snapshot_status == "available"):
+                LOG.debug("Full snapshot created.")
+                reporting.add_test_step("Create full snapshot", tvaultconf.PASS)
+            else:
+                LOG.error("Full snapshot creation failed.")
+                raise Exception("Create full snapshot")
+
+
+            #workload reassignment of same user and same tenant.
+            rc = self.workload_reassign(CONF.identity.tenant_id, workload_id, CONF.identity.user_id)
+            if rc == 0:
+                LOG.debug("Workload reassign to same user and same tenant is passed")
+                reporting.add_test_step(
+                    "Workload reassign to same user and same tenant", tvaultconf.PASS)
+            else:
+                LOG.error("Workload reassign to same user and same tenant is failed")
+                raise Exception("Workload reassign to same user and same tenant")
+
+            #check if actually tenant id is changed for the workload or not. 
+            workload_instance_info = self.get_workload_details(workload_id)
+            LOG.debug(f"workload_instance teanant id={workload_instance_info['project_id']} and tempest conf tenant id={CONF.identity.tenant_id}")
+            if CONF.identity.tenant_id == workload_instance_info['project_id']:
+                LOG.debug("Workload instance having correct tenant id. TEST CASE PASSED")
+            else:
+                LOG.error("Workload instance showing different tenant id. TEST CASE FAILED")
+                raise Exception("Workload reassign shows incorrect tenant id")
+
+        except Exception as e:
+            LOG.error("Exception: " + str(e))
+            reporting.add_test_step(str(e), tvaultconf.FAIL)
+            reporting.set_test_script_status(tvaultconf.FAIL)
+
+        finally:
+            reporting.test_case_to_write()
+
+
+    '''
+    OS-2059 - workload reassign with same user and different project.
+    http://192.168.15.51/testlink/linkto.php?tprojectPrefix=OS&item=testcase&id=OS-2059
+    '''
+    @decorators.attr(type='workloadmgr_api')
+    def test_03_workload_reassign(self):
+        try:
+
+            reporting.add_test_script(str(__name__) + "_same_user_different_tenant")
+
+            ### Create vm and workload ###
+            LOG.debug("Create VM")
+
+            self.created = False
+            vm_id = self.create_vm(vm_cleanup=True)
+            LOG.debug("\nVm id : {}\n".format(str(vm_id)))
+
+            LOG.debug("Create workload")
+            workload_id = self.workload_create(
+                [vm_id], tvaultconf.parallel, workload_cleanup=True)
+            LOG.debug("Workload ID: " + str(workload_id))
+
+            if(workload_id is not None):
+                self.wait_for_workload_tobe_available(workload_id)
+                if(self.getWorkloadStatus(workload_id) == "available"):
+                    reporting.add_test_step("Create workload", tvaultconf.PASS)
+                else:
+                    LOG.error("Failed to get workload status as available for workload ID: " + str(workload_id))
+                    raise Exception("Create workload")
+            else:
+                LOG.error("Failed to create workload.")
+                raise Exception("Create workload")
+
+            #take a full snapshot of it.
+            snapshot_id = self.create_snapshot(workload_id, is_full=True)
+
+            snapshot_status = self.getSnapshotStatus(workload_id, snapshot_id)
+
+            if (snapshot_status == "available"):
+                LOG.debug("Full snapshot created.")
+                reporting.add_test_step("Create full snapshot", tvaultconf.PASS)
+            else:
+                LOG.error("Full snapshot creation failed.")
+                raise Exception("Create full snapshot")
+
+
+            #use different project but same user and check the behavior.
+            rc = self.workload_reassign(CONF.identity.tenant_id_1, workload_id, CONF.identity.user_id)
+            if rc == 0:
+                LOG.debug("Workload reassign to same user and different tenant is passed")
+                reporting.add_test_step(
+                    "Workload reassign to same user and different tenant", tvaultconf.PASS)
+            else:
+                LOG.error("Workload reassign to same user and different tenant is failed")
+                raise Exception("Workload reassign to same user and different tenant")
+
+
+            #check if actually tenant id is changed for the workload or not. 
+            workload_instance_info = self.get_workload_details(workload_id)
+            LOG.debug(f"workload_instance teanant id={workload_instance_info['project_id']} and tempest conf tenant id={CONF.identity.tenant_id_1}")
+            if CONF.identity.tenant_id_1 == workload_instance_info['project_id']:
+                LOG.debug("Workload instance having correct tenant id. TEST CASE PASSED")
+            else:
+                LOG.error("Workload instance showing different tenant id. TEST CASE FAILED")
+                raise Exception("Workload reassign shows incorrect tenant id")
+
+        except Exception as e:
+            LOG.error("Exception: " + str(e))
+            reporting.add_test_step(str(e), tvaultconf.FAIL)
+            reporting.set_test_script_status(tvaultconf.FAIL)
+
+        finally:
+            reporting.test_case_to_write()
+
+
+
