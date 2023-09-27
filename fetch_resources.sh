3f633e281766680576f8e03fa982ef6d44f54db7
savitha-p <savitha.peri@trilio.io>
2023-09-27 15:02:10 +0530

    AUTO-385 get_mountpoint_path fix for kolla

--- fetch_resources.sh
+++ fetch_resources.sh
@@ -397,6 +397,7 @@ EOF
         dbusername=`echo $conn_str | cut -d '/' -f 3 | cut -d ':' -f 1`
         mysql_wlm_pwd=`echo $conn_str | cut -d '/' -f 3 | cut -d ':' -f 2 | cut -d '@' -f 1`
         dbname=`echo $conn_str | cut -d '/' -f 4 | cut -d '?' -f 1`
+	command_prefix="ssh root@$KOLLA_IP 'ssh $compute_hostname 'docker exec -t triliovault_datamover <command>''"
     else
         conn_str=`workloadmgr --insecure setting-list --get_hidden True -f value | grep sql_connection`
         mysql_ip=`echo $conn_str | cut -d '/' -f 3 | cut -d ':' -f 2 | cut -d '@' -f 2`
