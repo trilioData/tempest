1e1f923a3b358bb6253d7dea32817cc2f6820a84
Savitha Peri <savitha.peri@trilio.io>
2025-07-09 16:10:03 +0530

    AUTO-491 Enable functional run on RHOSO18

--- fetch_resources.sh
+++ fetch_resources.sh
@@ -419,6 +419,19 @@ EOF
         mysql_port=`ssh $HELM_USER@$HELM_IP "kubectl get svc -n openstack | grep mariadb-server" | xargs | cut -d ' ' -f5 | cut -d ':' -f2 | cut -d '/' -f1`
         echo 'wlm_dbport = '$mysql_port'' >> $TEMPEST_TVAULTCONF
 	command_prefix="ssh $HELM_USER@$HELM_IP '<command>'"
+    elif [[ ${OPENSTACK_DISTRO,,} == 'rhoso'* ]]
+    then
+        wlm_api_pod=`ssh $BASTION_USER@$BASTION_IP "oc get pods -n trilio-openstack | grep wlm-api | head -1" | cut -d ' ' -f1 | xargs`
+        conn_str=`ssh $BASTION_USER@$BASTION_IP "oc exec -t $wlm_api_pod -- grep sql_connection /tmp/pod-shared-triliovault-wlm-api/triliovault-wlm-dynamic.conf" | cut -d '=' -f2 | xargs`
+	mysql_node=`ssh $BASTION_USER@$BASTION_IP "oc get pods -n openstack -o wide  | grep openstack-galera | xargs | cut -d ' ' -f7"`
+        mysql_ip=`ssh $BASTION_USER@$BASTION_IP "oc get nodes -o wide | grep $mysql_node | xargs | cut -d ' ' -f6"`
+        echo "sql_connection: "$conn_str
+        dbusername=`echo $conn_str | cut -d '/' -f 3 | cut -d ':' -f 1`
+        mysql_wlm_pwd=`echo $conn_str | cut -d '/' -f 3 | cut -d ':' -f 2 | cut -d '@' -f 1`
+        dbname=`echo $conn_str | cut -d '/' -f 4 | cut -d '?' -f 1`
+        mysql_port=`ssh $BASTION_USER@$BASTION_IP "oc get svc -n openstack | grep galera-nodeport" | xargs | cut -d ' ' -f5 | cut -d ':' -f2 | cut -d '/' -f1`
+        echo 'wlm_dbport = '$mysql_port'' >> $TEMPEST_TVAULTCONF
+        command_prefix="ssh $BASTION_USER@$BASTION_IP \"ssh $COMPUTE_USER@$COMPUTE_IP '<command>'\""
     else
         conn_str=`workloadmgr --insecure setting-list --get_hidden True -f value | grep sql_connection`
         mysql_ip=`echo $conn_str | cut -d '/' -f 3 | cut -d ':' -f 2 | cut -d '@' -f 2`
